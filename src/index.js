de99d48bc726d68f28524a832d4dba0595df3282
Utsargo Roy <royutsargo@gmail.com>
2022-12-22 00:01:02 +0600

    Deploy site

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 /* eslint-disable no-unused-vars */
 
-import _ from "lodash";
-import "./style.scss";
+import _ from 'lodash';
+import './style.scss';
 
 // const gameId = "UGoHBGgZ60k4fkNyRsl5";
-let gameIdentity = "";
-fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games", {
-  method: "POST",
+let gameIdentity = '';
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+  method: 'POST',
   headers: {
-    "Content-type": "application/json; charset=UTF-8",
+    'Content-type': 'application/json; charset=UTF-8',
   },
   body: JSON.stringify({
-    name: "The game is on",
+    name: 'The game is on',
   }),
 })
   .then((res) => res.json())
   .then((gameID) => {
     const response = gameID.result;
-    gameIdentity = response.substring(14, response.lastIndexOf(" "));
+    gameIdentity = response.substring(14, response.lastIndexOf(' '));
   });
 const gameScores = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameIdentity}/scores`;
 
-const scoreList = document.getElementById("scores");
+const scoreList = document.getElementById('scores');
 const buildScoreList = async (gameLink) => {
   const response = await fetch(gameLink);
   await response.json().then((newData) => {
     scoreList.replaceChildren();
     newData.result.forEach((score) => {
-      const singleScore = document.createElement("li");
-      singleScore.classList.add("score");
+      const singleScore = document.createElement('li');
+      singleScore.classList.add('score');
       singleScore.innerHTML = `${score.user}: ${score.score}`;
       scoreList.appendChild(singleScore);
     });
@@ -36,9 +36,9 @@ const buildScoreList = async (gameLink) => {
 };
 const addScore = async (gameLink, user, score) => {
   await fetch(gameLink, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
     body: JSON.stringify({
       user,
@@ -47,18 +47,18 @@ const addScore = async (gameLink, user, score) => {
   });
 };
 
-const refreshBtn = document.getElementById("refresh-btn");
-refreshBtn.addEventListener("click", () => {
+const refreshBtn = document.getElementById('refresh-btn');
+refreshBtn.addEventListener('click', () => {
   buildScoreList(gameScores);
 });
 
-const userName = document.getElementById("user-name");
-const userScore = document.getElementById("user-score");
-const submitBtn = document.getElementById("submit-btn");
-const theForm = document.getElementById("add-score");
-submitBtn.addEventListener("click", (e) => {
+const userName = document.getElementById('user-name');
+const userScore = document.getElementById('user-score');
+const submitBtn = document.getElementById('submit-btn');
+const theForm = document.getElementById('add-score');
+submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  if (userName.value !== "" && userScore.value !== "") {
+  if (userName.value !== '' && userScore.value !== '') {
     addScore(gameScores, userName.value, userScore.value);
     buildScoreList(gameScores);
     theForm.reset();
